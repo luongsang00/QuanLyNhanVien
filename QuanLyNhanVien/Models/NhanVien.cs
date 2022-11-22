21ff1ebb2a59a323043000f39b6a9b36c5e9d050
Sang Lương Tiến <18520415@gm.uit.edu.vn>
2022-11-22 10:43:58 +0700

    Add project files.

---
+++ QuanLyNhanVien/Models/NhanVien.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuanLyNhanVien.Models
+{
+    public partial class NhanVien
+    {
+        public int Id { get; set; }
+        public string Ten { get; set; } = null!;
+        public string DiaChi { get; set; } = null!;
+        public string SoDienThoai { get; set; } = null!;
+    }
+}
---
+++ QuanLyNhanVien/Models/quanlynhanvienContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace QuanLyNhanVien.Models
+{
+    public partial class quanlynhanvienContext : DbContext
+    {
+        public quanlynhanvienContext()
+        {
+        }
+
+        public quanlynhanvienContext(DbContextOptions<quanlynhanvienContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<NhanVien> NhanViens { get; set; } = null!;
+
+        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseMySql("server=localhost;database=quanlynhanvien;userid=root", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.4.22-mariadb"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("utf8mb4_general_ci")
+                .HasCharSet("utf8mb4");
+
+            modelBuilder.Entity<NhanVien>(entity =>
+            {
+                entity.ToTable("nhan_vien");
+
+                entity.Property(e => e.Id)
+                    .HasColumnType("int(11)")
+                    .HasColumnName("id");
+
+                entity.Property(e => e.DiaChi).HasMaxLength(255);
+
+                entity.Property(e => e.SoDienThoai).HasMaxLength(255);
+
+                entity.Property(e => e.Ten).HasMaxLength(255);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
